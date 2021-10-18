a37f840467a853c0bbfe32c55822cff193910a5e
th34n <mingtheanlay@gmail.com>
2021-10-18 16:39:52 +0700

    finalize project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 // Angular Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 // App 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,12 +13,20 @@ import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
+import { AboutComponent } from './components/about/about.component';
 
 
 // Font Awesome
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: TasksComponent },
+  { path: 'about', component: AboutComponent },
+]
 
 @NgModule({
   declarations: [
@@ -26,14 +35,17 @@ import { FormsModule } from '@angular/forms';
     ButtonComponent,
     TasksComponent,
     TaskItemComponent,
-    AddTaskComponent
+    AddTaskComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,23 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  title = "Task Manager";
+  title: string = "Task Manager";
   date = new Date();
+  showAddTask: boolean = false;
+  subscription!: Subscription;
 
-  constructor() {
-    
+  constructor(private uiService: UiService, private router: Router) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
   }
 
-  myFunction(){
-    this.date=new Date();
-   }
+  dateTime() {
+    this.date = new Date();
+  }
 
   ngOnInit(): void {
 
   }
 
   toggledAddTask(): void {
-    console.log("Task added");
+    this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route;
   }
 
 }
